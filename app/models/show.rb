5b1134bae5a4d65b3f96f107cf0428da49b61e88
Sean Oughton <seanoughton@gmail.com>
2018-03-19 17:46:45 +0000

    automatically backed up by learn

---
+++ app/models/show.rb
@@ -0,0 +1,4 @@
+class Show < ActiveRecord::Base
+  def highest_rating
+  end
+end
---
+++ db/migrate/002_add_season_to_shows.rb
@@ -0,0 +1,8 @@
+#write a migration to add a column, season, to the shows table. The datatype of this column is string.
+
+
+class AddSeasonToShows < ActiveRecord::Migration
+  def change
+    add_column :shows, :season, :string
+  end
+end
